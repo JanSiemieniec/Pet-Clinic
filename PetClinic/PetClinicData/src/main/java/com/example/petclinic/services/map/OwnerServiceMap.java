c44d720ac2b2539b413a55e4fd46e7aecd7b1261
s22596 <s22596@pjwstk.edu.pl>
2023-07-19 18:10:59 +0200

    Adding bootstrap for data. closes #14.

--- PetClinic/PetClinicData/src/main/java/com/example/petclinic/services/map/OwnerServiceMap.java
+++ PetClinic/PetClinicData/src/main/java/com/example/petclinic/services/map/OwnerServiceMap.java
@@ -1,12 +1,13 @@
 package com.example.petclinic.services.map;
 
 import com.example.petclinic.model.Owner;
-import com.example.petclinic.services.CrudService;
+
+import com.example.petclinic.services.OwnerService;
 
 import java.util.Set;
 
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -32,4 +33,8 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     }
 
 
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
+    }
 }
