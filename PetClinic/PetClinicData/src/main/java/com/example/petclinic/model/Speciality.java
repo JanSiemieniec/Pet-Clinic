82cff4f637abd72113c87e2ee75b3db989f0b79a
s22596 <s22596@pjwstk.edu.pl>
2023-08-08 18:36:31 +0200

    Updated vet to use JPA. Closes #37

--- PetClinic/PetClinicData/src/main/java/com/example/petclinic/model/Speciality.java
+++ PetClinic/PetClinicData/src/main/java/com/example/petclinic/model/Speciality.java
@@ -1,7 +1,13 @@
 package com.example.petclinic.model;
 
-public class Speciality extends BaseEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "specialties")
+public class Speciality extends BaseEntity {
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
